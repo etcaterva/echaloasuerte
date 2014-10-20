7c20962db1fb2ed3b9f53cbef25e4a6577979e35
Mario Corchero <mariocj89@gmail.com>
2014-10-20 21:50:05 +0200

    first test

--- server/test/models/random_number_poll_test.py
+++ server/test/models/random_number_poll_test.py
@@ -1,4 +1,12 @@
 from django.test import TestCase
-
+from server.models import RandomNumberPoll
 # Create your tests here.
+class RandonNumberPollTestCase(TestCase):
+    def setUp(self):
+	pass
+
+    def build_random_number(self):
+        """Builds a random number pool"""
+	tested_item = RandomNumberPoll()
+	
 
