9afa42b72ba4c41560290067edda6a5c88bb4bbb
Mario Corchero <mariocj89@gmail.com>
2014-10-21 21:44:40 +0200

    Clean up

--- server/test/models/random_number_poll_test.py
+++ server/test/models/random_number_poll_test.py
@@ -7,15 +7,15 @@ class RandonNumberPollTestCase(TestCase):
 
     def build_random_number_test(self):
         """Builds a random number pool"""
-	tested_item = RandomNumberPoll()
+	    tested_item = RandomNumberPoll()
 
     def default_constructor_test(self):
-	"""Validate a randon number just constructed sets the default values"""
-	tested_item = RandomNumberPoll()
-	self.assertEqual(tested_item.range_min,0)
-	self.assertEqual(tested_item.range_max,None)
-	self.assertEqual(tested_item.number_of_results,1)
-	self.assertEqual(tested_item.allow_repeat,False)
+    	"""Validate a randon number just constructed sets the default values"""
+	    tested_item = RandomNumberPoll()
+    	self.assertEqual(tested_item.range_min,0)
+    	self.assertEqual(tested_item.range_max,None)
+    	self.assertEqual(tested_item.number_of_results,1)
+    	self.assertEqual(tested_item.allow_repeat,False)
 
     def parametrized_constructor_test(self):
         """Test parametrized constructor for RandonNumberPoll"""
@@ -26,21 +26,21 @@ class RandonNumberPollTestCase(TestCase):
         self.assertEqual(tested_item.allow_repeat,True)
 
     def is_feasible_default_test(self):
-	"""Default RandonNumberPoll is not feasible"""
-	self.assertFalse(RandomNumberPoll().is_feasible())
+    	"""Default RandonNumberPoll is not feasible"""
+    	self.assertFalse(RandomNumberPoll().is_feasible())
 
     def is_feasible_simple_test(self):
-	"""Simple RandonNumberPoll is feasible"""
-	tested_item = RandomNumberPoll(range_max=5)
-	self.assertTrue(tested_item.is_feasible())
+	    """Simple RandonNumberPoll is feasible"""
+        tested_item = RandomNumberPoll(range_max=5)
+    	self.assertTrue(tested_item.is_feasible())
 
     def is_feasible_limit_ok_test(self):
-	"""Border case for RandonNumberPoll is feasible OK"""
+    	"""Border case for RandonNumberPoll is feasible OK"""
         tested_item = RandomNumberPoll(range_max=5,range_min=2,number_of_results=3,allow_repeat=False)
         self.assertTrue(tested_item.is_feasible())
 
     def is_feasible_limit_ko_test(self):
-	"""Border case for RandonNumberPoll is feasible KO"""
+	    """Border case for RandonNumberPoll is feasible KO"""
         tested_item = RandomNumberPoll(range_max=5,range_min=2,number_of_results=4,allow_repeat=False)
         self.assertFalse(tested_item.is_feasible())
 
