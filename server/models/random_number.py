de5b83cc9fae22bd36125c69d9cdcc75b374f42e
Mario Corchero <mariocj89@gmail.com>
2014-10-21 23:17:56 +0200

    Add translation for prev commits

--- server/models/random_number.py
+++ server/models/random_number.py
@@ -1,22 +1,25 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 class RandonNumberDraw(models.Model):
     pass
+
+
 class RandomNumberPoll(models.Model):
     """
     Class that represents a poll with the details to produce random numbers.
     """
 
-    range_min = models.BigIntegerField("Range start", blank=False, null=False, default=0)
+    range_min = models.BigIntegerField(_("Range start"), blank=False, null=False, default=0)
     """"Minimun value to be generated. Inclusive."""
 
-    range_max = models.BigIntegerField("Range End", blank=False, null=False)
+    range_max = models.BigIntegerField(_("Range End"), blank=False, null=False)
     """"Maximun value to be generated. Exclusive."""
 
-    number_of_results = models.PositiveIntegerField("Number of results", blank=False, null=False, default=1)
+    number_of_results = models.PositiveIntegerField(_("Number of results"), blank=False, null=False, default=1)
     """Number of Random numbers to generate"""
 
-    allow_repeat = models.BooleanField("Allow Repetitions", blank=False, null=False, default=False)
+    allow_repeat = models.BooleanField(_("Allow Repetitions"), blank=False, null=False, default=False)
     """Whether the set of numbers to generate can contain repetitions. Note, if false, max-min > num_res"""
 
     def is_feasible(self):
