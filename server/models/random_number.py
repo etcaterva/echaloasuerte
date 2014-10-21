1f1e37e2a4c4f1739c5ad0ab3a56a2fd67350d44
Mario Corchero <mariocj89@gmail.com>
2014-10-21 21:08:01 +0200

    Basic Randon Number Model

--- server/models/random_number.py
+++ server/models/random_number.py
@@ -1,7 +1,29 @@
 from django.db import models
 
 class RandomNumberPoll(models.Model):
-	pass
+	"""
+	Class that represents a poll with the details to produce random numbers.
+	"""
+
+	range_min = models.BigIntegerField("Range start", blank=False, null=False, default=0)	
+	""""Minimun value to be generated. Inclusive."""
+
+	range_max = models.BigIntegerField("Range End", blank=False, null=False)
+	""""Maximun value to be generated. Exclusive."""
+
+	number_of_results = models.PositiveIntegerField("Number of results", blank=False, null=False, default=1)
+	"""Number of Random numbers to generate"""
+
+	allow_repeat = models.BooleanField("Allow Repetitions", blank=False, null=False, default=False)
+	"""Whether the set of numbers to generate can contain repetitions. Note, if false, max-min > num_res"""
+
+	def is_feasible(self):
+		if self.range_max is None: 
+			return False
+		if self.allow_repeat == True:
+			return True
+		else:
+			return self.range_max - self.range_min >= self.number_of_results
 
 class RandonNumberDraw(models.Model):
 	pass
--- server/test/models/random_number_poll_test.py
+++ server/test/models/random_number_poll_test.py
@@ -5,8 +5,46 @@ class RandonNumberPollTestCase(TestCase):
     def setUp(self):
 	pass
 
-    def build_random_number(self):
+    def build_random_number_test(self):
         """Builds a random number pool"""
 	tested_item = RandomNumberPoll()
 	
+    def default_constructor_test(self):
+	"""Validate a randon number just constructed sets the default values"""
+	tested_item = RandomNumberPoll()
+	self.assertEqual(tested_item.range_min,0)
+	self.assertEqual(tested_item.range_max,None)
+	self.assertEqual(tested_item.number_of_results,1)
+	self.assertEqual(tested_item.allow_repeat,False)
 
+    def parametrized_constructor_test(self):
+        """Test parametrized constructor for RandonNumberPoll"""
+        tested_item = RandomNumberPoll(range_max = 5,allow_repeat=True, number_of_results = 1)
+        self.assertEqual(tested_item.range_min,0)
+        self.assertEqual(tested_item.range_max,5)
+        self.assertEqual(tested_item.number_of_results,1)
+        self.assertEqual(tested_item.allow_repeat,True)
+
+    def is_feasible_default_test(self):
+	"""Default RandonNumberPoll is not feasible"""
+	self.assertFalse(RandomNumberPoll().is_feasible())
+
+    def is_feasible_simple_test(self):
+	"""Simple RandonNumberPoll is feasible"""
+	tested_item = RandomNumberPoll(range_max=5)
+	self.assertTrue(tested_item.is_feasible())
+
+    def is_feasible_limit_ok_test(self):
+	"""Border case for RandonNumberPoll is feasible OK"""
+        tested_item = RandomNumberPoll(range_max=5,range_min=2,number_of_results=3,allow_repeat=False)
+        self.assertTrue(tested_item.is_feasible())
+
+    def is_feasible_limit_ko_test(self):
+	"""Border case for RandonNumberPoll is feasible KO"""
+        tested_item = RandomNumberPoll(range_max=5,range_min=2,number_of_results=4,allow_repeat=False)
+        self.assertFalse(tested_item.is_feasible())
+
+    def is_feasible_limit_ko_with_repeat(self):
+        """Border case for RandonNumberPoll is feasible with repeat"""
+        tested_item = RandomNumberPoll(range_max=5,range_min=2,number_of_results=4,allow_repeat=True)
+        self.assertTrue(tested_item.is_feasible())
