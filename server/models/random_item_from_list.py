5f76aa8ad5cf6a279cfe85977a4328d7f6eb8fc1
David Naranjo <dnaranjo89@gmail.com>
2014-10-22 22:25:26 +0200

    Added fields to RandomItemFromListDraw

--- server/models/random_item_from_list.py
+++ server/models/random_item_from_list.py
@@ -5,7 +5,19 @@ class RandomItemFromListDraw(models.Model):
     """
     Class that represents a draw with the details choose random items from a list
     """
-    pass
+
+    #TODO Find the best way to store it in a model (Foreign Key, JSON...)
+    items = []
+    """List which store the items as strings"""
+
+    number_of_results = models.PositiveIntegerField(_("Number of results"), blank=False, null=False, default=1)
+    """Number of Random items to choose"""
+
+    allow_repeat = models.BooleanField(_("Allow Repetitions"), blank=False, null=False, default=False)
+    """Whether the set of items to generate can contain repetitions."""
+
+    def is_feasible(self):
+        return True
 
 
 class RandomItemFromListResult(models.Model):
