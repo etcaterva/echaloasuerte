2e444d86640e45e0ce61fbab75e194ad5d02d31b
David Naranjo <dnaranjo89@gmail.com>
2014-11-19 20:23:31 +0100

    DiceDraw view implemented

--- server/forms/__init__.py
+++ server/forms/__init__.py
@@ -1,3 +1,4 @@
 from server.forms.random_number_form import *
 from server.forms.random_item_form import *
 from server.forms.coin_form import *
+from server.forms.dice_form import *
--- web/views.py
+++ web/views.py
@@ -88,5 +88,28 @@ def coin_draw(request):
     context['draw'] = draw_form
     return render(request, 'coin.html', context)
 
+
+def dice_draw(request):
+    context = {}
+    if request.method == 'POST':
+        draw_form = DiceDrawForm(request.POST)
+        if draw_form.is_valid():
+            draw = draw_form.save()
+            if draw.is_feasible():
+                result = draw.toss()
+                list = []
+                for number in result.dice.all():
+                    list.append(number.value)
+                context = {'dice': list}
+            else:
+                print("The draw is not feasible!")
+        else:
+            print(draw_form.errors)
+    else:
+        draw_form = DiceDrawForm()
+
+    context['draw'] = draw_form
+    return render(request, 'dice.html', context)
+
 def under_construction(request):
     return render(request, 'under_construction.html', {})
