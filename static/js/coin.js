3bc78ced7d9fdb60d8ab949b17a730c368666bea
David Naranjo <dnaranjo89@gmail.com>
2014-11-25 00:50:59 +0100

    Fixup JS coin animation

--- static/js/coin.js
+++ static/js/coin.js
@@ -1,36 +1,48 @@
 var framenum = 0;
 var framecnt = 0;
 var flipping = null;
-var choice = 0;
 var pict = new Array(3, 4, 1, 4);
 var cachedimages = new Array(5);
-cachedimages[0] = new Image();
-cachedimages[0].src = "img/head.jpg";
-cachedimages[1] = new Image();
-cachedimages[1].src = "img/tail_dist.jpg";
-cachedimages[2] = new Image();
-cachedimages[2].src = "img/tail.jpg";
-cachedimages[3] = new Image();
-cachedimages[3].src = "img/head_dist.jpg";
-cachedimages[4] = new Image();
-cachedimages[4].src = "img/dist.jpg";
+
 
 /* 	choice = 0 (HEAH)
 	choice = 2 (TAIL)
 */
-function flip_coin(force_choice) {
+function coin() {}
+
+/* Pre: the baseUrl has to point to the folder with the images through static files*/
+coin.baseUrl = "";
+coin.result = 0;
+
+coin.setup = function () {
+    cachedimages[0] = new Image();
+    cachedimages[0].src = coin.baseUrl + "head.jpg";
+    cachedimages[1] = new Image();
+    cachedimages[1].src = coin.baseUrl + "tail_dist.jpg";
+    cachedimages[2] = new Image();
+    cachedimages[2].src = coin.baseUrl + "tail.jpg";
+    cachedimages[3] = new Image();
+    cachedimages[3].src = coin.baseUrl + "head_dist.jpg";
+    cachedimages[4] = new Image();
+    cachedimages[4].src = coin.baseUrl + "dist.jpg";
 	framecnt = 0;
-	choice = force_choice;
-	animate();
 }
-function animate() {
+
+
+coin.animate = function () {
 	framenum = (framecnt) % 4;
 	$("#img-coin" ).attr("src", cachedimages[pict[framenum]].src);
 	framecnt++;
-	if ((framecnt > 8) && (framenum == choice)) {
+	if ((framecnt > 8) && (framenum == coin.result)) {
 		$("#img-coin" ).attr("src", cachedimages[framenum].src);
 		flipping = null;
 	}
 	else
-		flipping = setTimeout("animate()", 30);
+		flipping = setTimeout("coin.animate()", 30);
+}
+
+
+coin.flip = function () {
+    coin.setup();
+	coin.animate();
 }
