838f553b1bf5e7f1c091e11a5e141b670ea75a75
David Naranjo <dnaranjo89@gmail.com>
2014-12-14 21:24:47 +0100

    Fixup: max attribute in inputs of RandomNumber now work properly

--- server/forms/random_number_form.py
+++ server/forms/random_number_form.py
@@ -8,10 +8,10 @@ from django.core.urlresolvers import reverse
 
 class RandomNumberDrawForm(forms.Form):
 
-    range_min = forms.IntegerField(_("From"), initial=0, required=True)
-    range_max = forms.IntegerField(_("To"), required=True)
-    number_of_results = forms.IntegerField(_("Number of results"), required=True, initial=1)
-    allow_repeat = forms.BooleanField(label=_("Allow repetitions"),required=False)
+    range_min = forms.IntegerField(label=_("From"), initial=0, required=True)
+    range_max = forms.IntegerField(label=_("To"), required=True)
+    number_of_results = forms.IntegerField(label=_("Number of results"), required=True, initial=1)
+    allow_repeat = forms.BooleanField(label=_("Allow repetitions"), required=False)
 
     def __init__(self, *args, **kwargs):
         super(RandomNumberDrawForm, self).__init__(*args, **kwargs)
