7d6ef7f62ac6ae5867145be19d82f1eca1c53184
David Naranjo <dnaranjo89@gmail.com>
2014-12-15 23:51:38 +0100

    Showing draw errors in RandomNumber

--- server/forms/random_number_form.py
+++ server/forms/random_number_form.py
@@ -1,5 +1,5 @@
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Row, Div
+from crispy_forms.layout import Layout, Submit, Row, HTML
 
 from django import forms
 from django.utils.translation import ugettext_lazy as _
@@ -15,18 +15,19 @@ class RandomNumberDrawForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(RandomNumberDrawForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        # self.helper.field_template = 'bootstrap3/eas_field.html'
+        self.helper.field_template = 'eas_field.html'
         self.helper.form_id = 'form-random_number'
         self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'col-xs-6 col-sm-4 text-right'
-        self.helper.field_class = 'col-xs-4'
+        self.helper.label_class = 'col-xs-6 col-md-4 text-right'
+        self.helper.field_class = 'col-xs-3'
         self.helper.form_class = 'blueForms'
         self.helper.form_method = 'post'
         self.helper.form_action = reverse('random_number')
         self.helper.layout = Layout(
-            Row('number_of_results'),
             Row('range_min'),
             Row('range_max'),
+            Row('number_of_results'),
             'allow_repeat',
+            HTML("{% include 'draw_errors.html' %}"),
             Row(Submit('submit', _("Toss"), css_class='btn btn-primary'), css_class='text-center'),
         )
