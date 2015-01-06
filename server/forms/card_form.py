33a580d60f2b618c04f91c217558c9ce42da22e9
David Naranjo <dnaranjo89@gmail.com>
2015-01-07 00:51:08 +0100

    CardDraw working

--- server/forms/card_form.py
+++ server/forms/card_form.py
@@ -7,7 +7,7 @@ from django.core.urlresolvers import reverse
 
 class CardDrawForm(forms.Form):
     number_of_results = forms.IntegerField(label=_("Number of results"), required=True, initial=1, max_value=20)
-    type_of_deck = forms.CharField(label=_("Type of deck"), required=True)
+    type_of_deck = forms.CharField(label=_("Type of deck"), required=True, initial="french")
 
     def __init__(self, *args, **kwargs):
         super(CardDrawForm, self).__init__(*args, **kwargs)
---
+++ static/js/card.js
@@ -0,0 +1,16 @@
+
+function Card() {}
+
+/* Pre: the baseUrl has to point to the folder with the images through static files*/
+Card.baseUrl = "";
+Card.result = 0;
+Card.css_class = "col-xs-6 col-sm-4 col-lg-3";
+
+Card.draw = function(results){
+    Card.baseUrl += "img_cards/";
+    for (i = 0; i < results.length; i++) {
+        img_path = Card.baseUrl + results[i] + ".png";
+        string_img = "<div class='" + Card.css_class + "'><img class='img-responsive' src='" + img_path + "'/></div>";
+        document.write(string_img);
+    }
+}
