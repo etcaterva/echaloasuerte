9f78698308002e0ac08c6bd4062ff3a9b079b473
David Naranjo <dnaranjo89@gmail.com>
2015-01-11 19:03:11 +0100

    Modified bootstrap columns for card results

--- static/js/card.js
+++ static/js/card.js
@@ -3,14 +3,33 @@ function Card() {}
 
 /* Pre: the baseUrl has to point to the folder with the images through static files*/
 Card.baseUrl = "";
-Card.result = 0;
-Card.css_class = "col-xs-6 col-sm-4 col-lg-3 card-container";
+Card.results = [];
+Card.css_class = "card-container ";
 
-Card.draw = function(results){
-    Card.baseUrl += "img_cards/";
-    for (i = 0; i < results.length; i++) {
-        img_path = Card.baseUrl + results[i] + ".png";
-        string_img = "<div class='" + Card.css_class + "'><img src='" + img_path + "'/></div>";
-        document.write(string_img);
+Card.setup = function(baseUrl, results){
+    Card.baseUrl = baseUrl + "img_cards/";
+    Card.results = results;
+
+    switch (Card.results.length) {
+        case 1:
+            Card.css_class += "col-xs-12";
+            break;
+        case 2:
+            Card.css_class += "col-xs-6";
+            break;
+        case 3:
+            Card.css_class += "col-xs-6 col-sm-4";
+            break;
+        default:
+            Card.css_class += "col-xs-6 col-sm-3";
+            break;
+    }
+}
+
+Card.draw = function(){
+    for (i = 0; i < Card.results.length; i++) {
+        img_path = Card.baseUrl + Card.results[i] + ".png";
+        html = "<div class='" + Card.css_class + "'><img src='" + img_path + "'/></div>";
+        document.write(html);
     }
 }
