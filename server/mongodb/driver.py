206f6ffc10f8b74c32314e18626eddbc591090d4
David Naranjo <dnaranjo89@gmail.com>
2015-01-21 23:37:09 +0100

    LInked_sets added to mongo driver

--- server/mongodb/driver.py
+++ server/mongodb/driver.py
@@ -5,6 +5,7 @@ from server.bom.coin import *
 from server.bom.dice import *
 from server.bom.random_number import *
 from server.bom.card import *
+from server.bom.link_sets import *
 from server.bom.random_item import *
 from server.bom.user import *
 
