b674ff499993228f620a07fb576f4a28b012aa95
David Naranjo <dnaranjo89@gmail.com>
2015-01-27 19:55:38 +0100

    Added more tests to RandomNumberForm

--- server/test/forms/random_number_form_test.py
+++ server/test/forms/random_number_form_test.py
@@ -1,13 +1,46 @@
 from django.test import TestCase
 from server.forms.random_number_form import RandomNumberDrawForm
 
+
 class RandomNumberDrawFormTest(TestCase):
-    def invalid_range_test(self):
+    def feasible_ok_test(self):
+        """RandomNumberDraw: Correct draw is feasible"""
+        form_data = {'range_min': 1, 'range_max': 4, 'number_of_results': 1}
+        form = RandomNumberDrawForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def invalid_range_ko_test(self):
+        """RandomNumberDraw: Invalid range not feasible"""
         form_data = {'range_min': 5, 'range_max': 4, 'number_of_results': 1}
         form = RandomNumberDrawForm(data=form_data)
         self.assertFalse(form.is_valid())
 
-    def range_too_small_test(self):
+    def invalid_range_with_repeat_ko_test(self):
+        """RandomNumberDraw: Invalid range with repeat not feasible"""
+        form_data = {'range_min': 5, 'range_max': 4, 'number_of_results': 1, 'allow_repeat': True}
+        form = RandomNumberDrawForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def range_too_small_ko_test(self):
+        """RandomNumberDraw: Too small range not feasible"""
         form_data = {'range_min': 1, 'range_max': 5, 'number_of_results': 10}
+        form = RandomNumberDrawForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def range_small_with_repeat_ok_test(self):
+        """RandomNumberDraw: Small range with repeat is feasible"""
+        form_data = {'range_min': 1, 'range_max': 5, 'number_of_results': 10, 'allow_repeat': True}
+        form = RandomNumberDrawForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def too_many_results_ko_test(self):
+        """RandomNumberDraw: Too many results requested (over the limit) is not feasible"""
+        form_data = {'range_min': 1, 'range_max': 5, 'number_of_results': 60}
+        form = RandomNumberDrawForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def not_enough_results_ko_test(self):
+        """RandomNumberDraw: Number of numbers requested less than one is not feasible"""
+        form_data = {'range_min': 1, 'range_max': 5, 'number_of_results': 0}
         form = RandomNumberDrawForm(data=form_data)
         self.assertFalse(form.is_valid())
