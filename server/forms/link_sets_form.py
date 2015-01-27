993b708ed0616ccf053d7735a1bdaa9d1b225b23
David Naranjo <dnaranjo89@gmail.com>
2015-01-27 22:56:33 +0100

    Refactored linkSet draw template

--- server/forms/link_sets_form.py
+++ server/forms/link_sets_form.py
@@ -1,8 +1,8 @@
 from django import forms
-from django.forms.models import modelformset_factory
 from django.utils.translation import ugettext_lazy as _
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Row, Div, Field
+from crispy_forms.layout import Layout, Div, Submit
+from django.core.urlresolvers import reverse
 
 
 class LinkSetsForm(forms.Form):
@@ -11,17 +11,20 @@ class LinkSetsForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(LinkSetsForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.form_tag = False
         self.helper.form_id = 'form-link_sets'
         self.helper.form_class = 'form-horizontal'
         self.helper.label_class = 'col-xs-3'
         self.helper.field_class = 'col-xs-9'
         self.helper.form_class = 'blueForms'
         self.helper.form_method = 'post'
-        self.helper.form_action = '/link_sets'
+        self.helper.form_action = reverse('link_sets')
         self.helper.layout = Layout(
             Div(
                 'set_1',
                 'set_2',
             ),
+            Div(
+                Submit('submit', _("Toss"), css_class='btn-toss'),
+                css_class='text-center',
+            )
         )
