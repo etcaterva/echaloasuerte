ba4476124f6f4b6de0fc2d04592beb5a35c5cf9d
Mario Corchero <mariocj89@gmail.com>
2015-01-30 20:56:17 +0000

    fix language

--- echaloasuerte/settings/common.py
+++ echaloasuerte/settings/common.py
@@ -57,7 +57,7 @@ TIME_ZONE = 'UTC'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html.
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 LANGUAGES = (
           ('es', _('Spanish')),
           ('en', _('English')),
