4c61543f108b3a9f3653d232a03d8ed327b95778
David Naranjo <dnaranjo89@gmail.com>
2015-03-24 18:23:14 +0100

    Add "protected" CSS class to set these inputs as read-only when required

--- server/forms/dice_form.py
+++ server/forms/dice_form.py
@@ -11,6 +11,9 @@ class DiceDrawForm(FormBase):
     def __init__(self, *args, **kwargs):
         super(DiceDrawForm, self).__init__(*args, **kwargs)
 
+        # Add "protected" class to the input that will be read-only when the draw is public
+        self.fields['number_of_results'].widget.attrs.update({'class': 'protected'})
+
         self.helper.label_class = 'col-xs-7 text-right'
         self.helper.field_class = 'col-xs-5'
         self.helper.layout = Layout(
