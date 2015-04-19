67fdfb2d77ed2b611458dda424d2f70269f34ebc
Mario Corchero <mariocj89@gmail.com>
2015-04-19 23:00:49 +0100

    Remove legacy app code

--- legacy-app/web-2011/en/php/confirmar.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$usuario = isset($_GET['usuario']) ? $_GET['usuario'] : $_POST['usuario'];
-
-	header('Content-Type: text/html; charset=utf-8');
-    $conn = mysql_connect("localhost","root","toor");
-    mysql_select_db("echaloasuerte", $conn);
-
-	mysql_query("UPDATE USUARIO SET checked = 1 where id = ".$usuario);
-  ?>
