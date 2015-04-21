5976601505b56cfea170c70fb4f4dd6d82014378
David Naranjo <dnaranjo89@gmail.com>
2015-04-21 17:29:25 +0200

    Refactor in PublicDraw JS code

--- static/js/public_draw_manager.js
+++ static/js/public_draw_manager.js
@@ -1,7 +1,7 @@
-var public_draw_manager = {};
+var PublicDraw = {};
 
 // Updates the breadcrumb to show the steps that have been already done
-public_draw_manager.update_breadcrumb = function (current_step){
+PublicDraw.update_breadcrumb = function (current_step){
     // However we reached here, the step "choose it" has already been done
     $('.info-public-draw #choose').addClass('done');
     if (current_step == "spread"){
@@ -16,7 +16,7 @@ public_draw_manager.update_breadcrumb = function (current_step){
 }
 
 // Store the step we are in (when creating a public draw) to send it in the POST
-public_draw_manager.set_submition_type = function (current_step){
+PublicDraw.set_submition_type = function (current_step){
     var submit_type = "public_toss";
     if (current_step == "configure"){
         submit_type = "go_to_spread";
@@ -34,7 +34,7 @@ public_draw_manager.set_submition_type = function (current_step){
 
 // Thi function runs when the user make changes in the privacy of a public draw and click "Save" button
 // It store the corresponding values in the input field which will be POSTed
-public_draw_manager.update_privacy_fields = function (){
+PublicDraw.update_privacy_fields = function (){
     var $shared_type_field = $('input#shared-type');
     var mode = $('#privacy-selector').attr('data-selected');
     if (mode == "invited"){
@@ -51,18 +51,17 @@ public_draw_manager.update_privacy_fields = function (){
 }
 
 //Initialize the UI to select the level of privacy for the draw
-public_draw_manager.prepare_privacy_selection = function (){
+PublicDraw.prepare_privacy_selection = function (){
     // Initialize the UI (slider) to choose the level of restriction of the public draw
     $("#privacy-selector").slideSelector();
 
     // Initialize button "Save changes". It stores the selection in the form input
     $('button#save-change-privacy').click(function () {
-        public_draw_manager.update_privacy_fields();
+        PublicDraw.update_privacy_fields();
     });
 }
 
-public_draw_manager.settings = function () {
-
+PublicDraw.settings = function () {
     // Update the slide selector to show the current level of privacy
     function initialize_slideselector () {
         var current_privacy_level = $('input#shared-type').val();
@@ -83,65 +82,85 @@ public_draw_manager.settings = function () {
         }
     }
 
-    function main_screen_settings () {
+    function init_settings_panel(){
+        // Show the main settings screen
         $('#settings-general').removeClass("hide");
         $('.settings-submenu').addClass("hide");
-    };
 
-    function close_settings () {
-        $('#public-draw-settings').modal('hide');
-        main_screen_settings();
-    };
+        // Remove previous feedback in case they exist
+        $('div#public-draw-settings div.feedback').addClass('hide');
 
+        initialize_slideselector();
+    }
+
+    // Open settings panel
+    $('#settings-button').click(function (){
+        init_settings_panel();
+    });
+
+    // Show the settings' main screen
+    $('.btn-settings-back').click(function (){
+        init_settings_panel();
+    });
+
+    /*
+    SETTINGS OPTION: Edit draw
+    Show the confirmation dialog before beginning the edition
+    */
     $('li#edit-draw').click(function() {
         $('#settings-general').addClass("hide");
         $('#settings-edit-draw').removeClass("hide");
     });
 
-    // Set up the UI to edit a public draw (already published)
-    // Unlock the fields, hide toss button and present buttons to save changes and cancel the edition
-    $('a#btn-edit-draw').click(function() {
-        public_draw_manager.unlock_fields();
+    /*
+    SETTINGS OPTION: Edit draw
+    Set up the UI to edit a public draw (already published)
+    Unlock the fields, hide toss button and present buttons to save changes and cancel the edition
+    */
+    $('a#edit-draw-confirmation').click(function() {
+        PublicDraw.unlock_fields();
+        // Hide the toss button
         $('button#toss').addClass('hide');
+        // Show the "Save changes" and "Cancel edition" buttons
         $('div#edit-draw-save-changes').removeClass('hide');
-        close_settings();
+        // Close settings panel
+        $('#public-draw-settings').modal('hide');
+        // Disable Settings button
+        $('settings-button').prop( "disabled", true );
     });
 
-    // When the user is editing a public draw, the buttons "Cancel edition" and "Save changes" are presented.
-    // That's the first cancel button
+    /*
+    SETTINGS OPTION: Edit draw
+    If the user cancel the draw edition, the page is reloaded.
+    */
     $('a#edit-draw-cancel').click(function() {
         // using replace instead on reload to avoid unintentional form submissions
         var url = window.location.href;
         window.location.replace(url);
     });
 
-    // When the user is editing a public draw, the buttons "Cancel edition" and "Save changes" are presented.
-    // That's the first "Save changes" button
+    /*
+    SETTINGS OPTION: Edit draw
+    If the has edited a public draw so the configuration is submited to the server
+    */
     $('button#edit-draw-save').click(function() {
         $("input[name=submit-type]").val("edit_public_draw");
         return true;
     });
 
-
-
-
+    /*
+    SETTINGS OPTION: Invite users
+    Show the invitation panel
+    */
     $('li#invite').click(function() {
         $('#settings-general').addClass("hide");
         $('#settings-invite').removeClass("hide");
     });
 
-    $('li#privacy').click(function() {
-        initialize_slideselector();
-        $('#settings-general').addClass("hide");
-        $('#settings-privacy').removeClass("hide");
-    });
-
-    $('.btn-settings-back').click(function() {
-        main_screen_settings();
-        // Hide the alters which are showing a response from the server
-        $('div#public-draw-settings div.feedback').addClass('hide');
-    });
-
+    /*
+    SETTINGS OPTION: Invite users
+    Send the new users to the server
+    */
     $('a#send-emails').click(function() {
         $('div#settings-invite div.feedback').addClass('hide');
         var draw_id = $(this).attr("data-id");
@@ -150,7 +169,7 @@ public_draw_manager.settings = function () {
         // Store the emails in the draw form input
         $('#users').val(users);
 
-        $.get(public_draw_manager.url_invite_users, {draw_id: draw_id, emails: users}, function(data){
+        $.get(PublicDraw.url_invite_users, {draw_id: draw_id, emails: users}, function(data){
             $('div#alert-invitation-success').removeClass('hide');
         })
         .fail(function() {
@@ -158,13 +177,27 @@ public_draw_manager.settings = function () {
         });
     });
 
+    /*
+    SETTINGS OPTION: Change privacy
+    Show the privacy panel
+    */
+    $('li#privacy').click(function() {
+        initialize_slideselector();
+        $('#settings-general').addClass("hide");
+        $('#settings-privacy').removeClass("hide");
+    });
+
+    /*
+    SETTINGS OPTION: Change privacy
+    Confirmation after editing privacy settings
+    */
     $('button#save-change-privacy').click(function() {
         $('div#settings-privacy div.feedback').addClass('hide');
-        public_draw_manager.update_privacy_fields();
+        PublicDraw.update_privacy_fields();
         var draw_id = $(this).attr("data-id");
         var shared_type = $('input#shared-type').val();
         var password = $('input#id_password').val();
-        $.get(public_draw_manager.url_draw_privacy, {draw_id: draw_id, shared_type: shared_type, password: password}, function(data){
+        $.get(PublicDraw.url_draw_privacy, {draw_id: draw_id, shared_type: shared_type, password: password}, function(data){
             /* The delay has the purpose to show "refresh" feedback when a to consecutive changes success */
             $('div#alert-level-privacy-success').removeClass('hide',100);
         })
@@ -172,10 +205,9 @@ public_draw_manager.settings = function () {
             $('div#alert-level-privacy-failed').removeClass('hide',100);
         });
     });
-
 }
 
-public_draw_manager.lock_fields = function () {
+PublicDraw.lock_fields = function () {
     // Add read-only property to the inputs of the draw
     $('.protected').prop('readonly', true);
 
@@ -184,7 +216,7 @@ public_draw_manager.lock_fields = function () {
     $('.protected').parent('.tokenfield').attr('readonly', "true");
 }
 
-public_draw_manager.unlock_fields = function () {
+PublicDraw.unlock_fields = function () {
     // Add read-only property to the inputs of the draw
     $('.protected').removeProp('readonly');
 
@@ -193,7 +225,7 @@ public_draw_manager.unlock_fields = function () {
     $('.protected').parent('.tokenfield').removeAttr('readonly');
 }
 
-public_draw_manager.prepare_invitation_fields = function () {
+PublicDraw.prepare_invitation_fields = function () {
     $('#publish').click(function () {
         var users_to_invite = $('#invite-emails').val();
         $('#users').val(users_to_invite);
@@ -201,23 +233,23 @@ public_draw_manager.prepare_invitation_fields = function () {
 };
 
 // Initialize the interface for a public draw
-public_draw_manager.setup = function(current_step){
-    public_draw_manager.set_submition_type(current_step);
+PublicDraw.setup = function(current_step){
+    PublicDraw.set_submition_type(current_step);
     //Initialize the UI to select the level of privacy for the draw
-    public_draw_manager.prepare_privacy_selection();
+    PublicDraw.prepare_privacy_selection();
 
-    public_draw_manager.prepare_invitation_fields();
+    PublicDraw.prepare_invitation_fields();
 
     // Initialize input to submit emails to be shown as a tokenField
     $('input#invite-emails').tokenfield({createTokensOnBlur:true, delimiter: [',',' '], inputType: 'email', minWidth: 300});
 
     if (current_step == ""){
         // If the draw has already been published
-        public_draw_manager.settings();
-        public_draw_manager.lock_fields();
+        PublicDraw.settings();
+        PublicDraw.lock_fields();
     } else{
         // If the the user is creating the draw
-        public_draw_manager.update_breadcrumb(current_step);
+        PublicDraw.update_breadcrumb(current_step);
         if (current_step == "spread"){
             $('.step-configure').hide();
         } else if (current_step == "configure"){
