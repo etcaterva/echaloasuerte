1c0944a62286e46907acfc4bc186b4127ebd19c5
David Naranjo <dnaranjo89@gmail.com>
2015-04-27 22:04:37 +0200

    fixup tests

--- func_tests/signed_up_tests.py
+++ func_tests/signed_up_tests.py
@@ -118,7 +118,6 @@ class SanityWebapp(SeleniumTest):
         - The draw can be accessed by non-register users by using the password
         - Only the owner can toss
         """
-        # comment
         driver_signed_in = self.driver_signed_in
         driver = self.driver
         password = '123456'
@@ -126,7 +125,7 @@ class SanityWebapp(SeleniumTest):
         driver_signed_in.find_element_by_css_selector("#public-draw-dropdown .public-draw-create").click()
         driver_signed_in.find_element_by_css_selector("#dice-draw").click()
         driver_signed_in.find_element_by_name("title").clear()
-        driver_signed_in.find_element_by_name("title").send_keys("Public draw test2")
+        driver_signed_in.find_element_by_name("title").send_keys("Public draw test")
         driver_signed_in.find_element_by_id("next").click()
         driver_signed_in.find_element_by_id("public-mode-selected").click()
         time.sleep(1)
@@ -139,14 +138,6 @@ class SanityWebapp(SeleniumTest):
         driver_signed_in.find_element_by_id("publish").click()
         driver_signed_in.find_element_by_id("toss").click()
         draw_id = driver_signed_in.find_element_by_id("id__id").get_attribute('value')
-        '''
-        driver_signed_in.find_element_by_css_selector("div.slider-tick.position-3").click()
-        #driver_signed_in.find_element_by_id("draw-password").clear()
-        #driver_signed_in.find_element_by_id("draw-password").send_keys(password)
-        driver_signed_in.find_element_by_id("save-change-privacy").click()
-        driver_signed_in.find_element_by_id("publish").click()
-        draw_id = driver_signed_in.find_element_by_id("id__id").get_attribute('value')
-        driver_signed_in.find_element_by_id("toss").click()'''
 
         # Access the draw using the password
         driver.find_element_by_id("public-draw").click()
