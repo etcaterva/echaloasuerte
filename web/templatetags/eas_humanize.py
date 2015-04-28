71c7b50154e3cdd167bef3868d808757dbceb37f
David Naranjo <dnaranjo89@gmail.com>
2015-04-28 05:48:08 +0200

    Unified join draw tables and improved layout

---
+++ web/templatetags/eas_humanize.py
@@ -0,0 +1,131 @@
+# -*- encoding: utf-8 -*-
+from __future__ import unicode_literals
+import datetime
+from django import template
+from django.template import defaultfilters
+from django.utils.translation import pgettext, ungettext, ugettext as _
+from django.utils.html import avoid_wrapping
+from django.utils.timezone import is_aware, utc
+from django.utils.translation import ugettext, ungettext_lazy
+
+register = template.Library()
+
+def timesince(d, now=None, reversed=False):
+    """
+    Takes two datetime objects and returns the time between d and now
+    as a nicely formatted string, e.g. "10 minutes".  If d occurs after now,
+    then "0 minutes" is returned.
+
+    Units used are years, months, weeks, days, hours, and minutes.
+    Seconds and microseconds are ignored.  Up to two adjacent units will be
+    displayed.  For example, "2 weeks, 3 days" and "1 year, 3 months" are
+    possible outputs, but "2 weeks, 3 hours" and "1 year, 5 days" are not.
+
+    Adapted from
+    http://web.archive.org/web/20060617175230/http://blog.natbat.co.uk/archive/2003/Jun/14/time_since
+    """
+    chunks = (
+        (60 * 60 * 24 * 365, ungettext_lazy('%d year', '%d years')),
+        (60 * 60 * 24 * 30, ungettext_lazy('%d month', '%d months')),
+        (60 * 60 * 24 * 7, ungettext_lazy('%d week', '%d weeks')),
+        (60 * 60 * 24, ungettext_lazy('%d day', '%d days')),
+        (60 * 60, ungettext_lazy('%d hour', '%d hours')),
+        (60, ungettext_lazy('%d minute', '%d minutes'))
+    )
+    # Convert datetime.date to datetime.datetime for comparison.
+    if not isinstance(d, datetime.datetime):
+        d = datetime.datetime(d.year, d.month, d.day)
+    if now and not isinstance(now, datetime.datetime):
+        now = datetime.datetime(now.year, now.month, now.day)
+
+    if not now:
+        now = datetime.datetime.now(utc if is_aware(d) else None)
+
+    delta = (d - now) if reversed else (now - d)
+    # ignore microseconds
+    since = delta.days * 24 * 60 * 60 + delta.seconds
+    if since <= 0:
+        # d is in the future compared to now, stop processing.
+        return avoid_wrapping(ugettext('0 minutes'))
+    for i, (seconds, name) in enumerate(chunks):
+        count = since // seconds
+        if count != 0:
+            break
+    result = avoid_wrapping(name % count)
+    if count <= 1 and i + 1 < len(chunks):
+        # Now get the second item
+        seconds2, name2 = chunks[i + 1]
+        count2 = (since - (seconds * count)) // seconds2
+        if count2 != 0:
+            result += ugettext(', ') + avoid_wrapping(name2 % count2)
+    return result
+
+# This filter doesn't require expects_localtime=True because it deals properly
+# with both naive and aware datetimes. Therefore avoid the cost of conversion.
+@register.filter
+def naturaltime(value):
+    """
+    For date and time values shows how many seconds, minutes or hours ago
+    compared to current timestamp returns representing string.
+    """
+    if not isinstance(value, datetime.date):  # datetime is a subclass of date
+        return value
+
+    now = datetime.datetime.now(utc if is_aware(value) else None)
+    if value < now:
+        delta = now - value
+        if delta.days != 0:
+            return pgettext(
+                'naturaltime', '%(delta)s ago'
+            ) % {'delta': timesince(value, now)}
+        elif delta.seconds == 0:
+            return _('now')
+        elif delta.seconds < 60:
+            return ungettext(
+                # Translators: please keep a non-breaking space (U+00A0)
+                # between count and time unit.
+                'a second ago', '%(count)s seconds ago', delta.seconds
+            ) % {'count': delta.seconds}
+        elif delta.seconds // 60 < 60:
+            count = delta.seconds // 60
+            return ungettext(
+                # Translators: please keep a non-breaking space (U+00A0)
+                # between count and time unit.
+                'a minute ago', '%(count)s minutes ago', count
+            ) % {'count': count}
+        else:
+            count = delta.seconds // 60 // 60
+            return ungettext(
+                # Translators: please keep a non-breaking space (U+00A0)
+                # between count and time unit.
+                'an hour ago', '%(count)s hours ago', count
+            ) % {'count': count}
+    else:
+        delta = value - now
+        if delta.days != 0:
+            return pgettext(
+                'naturaltime', '%(delta)s from now'
+            ) % {'delta': defaultfilters.timeuntil(value, now)}
+        elif delta.seconds == 0:
+            return _('now')
+        elif delta.seconds < 60:
+            return ungettext(
+                # Translators: please keep a non-breaking space (U+00A0)
+                # between count and time unit.
+                'a second from now', '%(count)s seconds from now', delta.seconds
+            ) % {'count': delta.seconds}
+        elif delta.seconds // 60 < 60:
+            count = delta.seconds // 60
+            return ungettext(
+                # Translators: please keep a non-breaking space (U+00A0)
+                # between count and time unit.
+                'a minute from now', '%(count)s minutes from now', count
+            ) % {'count': count}
+        else:
+            count = delta.seconds // 60 // 60
+            return ungettext(
+                # Translators: please keep a non-breaking space (U+00A0)
+                # between count and time unit.
+                'an hour from now', '%(count)s hours from now', count
+            ) % {'count': count}
+
