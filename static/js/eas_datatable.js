2c2ff3b94041e8bbdd2ca833f57ad80605fe9b60
David Naranjo <dnaranjo89@gmail.com>
2015-05-03 22:05:49 +0200

    tablas

--- static/js/eas_datatable.js
+++ static/js/eas_datatable.js
@@ -4,7 +4,10 @@
     var pluginName = 'EASDataTable',
         defaults = {
             dataTable_plugin: null,
-            msg_your_draws: ""
+            msg_your_draws: "",
+            msg_search: "",
+            msg_show: "Show",
+            msg_entries: "entries"
         };
 
     /*********************************
@@ -24,6 +27,7 @@
             this.options = $.extend( {}, defaults, options) ;
 
             this.checkbox_filter_your_draws();
+            this.add_bootstrap();
         },
 
         checkbox_filter_your_draws: function(){
@@ -41,6 +45,26 @@
                     that.options.dataTable_plugin.column(5).search("").draw();
                 }
             });
+        },
+
+        add_bootstrap: function(){
+            var html_search_input = '<div class="input-group">' +
+                                        '<input id="public-draw-search" class="form-control" type="text" placeholder="' + this.options.msg_search + '">' +
+                                        '<span class="input-group-btn"><button class="btn btn-default" type="button"><span class="fa fa-search"></span></button></span>';
+            this.$element.find('#public-draws_filter').empty();
+            this.$element.find('#public-draws_filter').append(html_search_input);
+
+            // Select input to choose the number of results
+            var html_select = this.$element.find('select').outerHTML();
+            html_select =  '<label for="inputKey" class="col-md-1 control-label">Show</label>' +
+                            '<label id="entry-label" for="inputKey" class="col-md-1 control-label">entries</label>' +
+                            '<div class="col-md-10"> ' + html_select + '</div>';
+
+            console.log(html_select);
+            this.$element.find('.dataTables_length').empty();
+            this.$element.find('.dataTables_length').append(html_select);
+            this.$element.find('.dataTables_length').append('<p class="form-control-static"></p>');
+            this.$element.find('select').addClass("form-control");
         }
     };
 
@@ -62,6 +86,10 @@
             }
         })
     }
+
+    $.fn.outerHTML = function() {
+      return jQuery('<div />').append(this.eq(0).clone()).html();
+    };
 
 })(window.jQuery, window, document );
 
