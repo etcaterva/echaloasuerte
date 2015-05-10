2666e9894e6d610150ab8208363db5f20919f32f
David Naranjo <dnaranjo89@gmail.com>
2015-05-10 18:30:24 +0200

    Store templates in cache only in production

--- echaloasuerte/settings/prod.py
+++ echaloasuerte/settings/prod.py
@@ -94,3 +94,18 @@ LOGGING = {
 MONGO_END_POINTS = [
         {'host':'localhost', 'port':27017, 'database':'prod'},
         ]
+
+# Template caching
+TEMPLATE_LOADERS = (
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
+)
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'TIMEOUT': 60 # 1 MINUTE
+    }
+}
