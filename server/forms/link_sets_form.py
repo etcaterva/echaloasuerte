17bd4893c93ce4202be52b4502a272a7724c10f7
David Naranjo <dnaranjo89@gmail.com>
2015-06-19 19:24:45 +0200

    Rename class to initialize tokenfields

--- server/forms/link_sets_form.py
+++ server/forms/link_sets_form.py
@@ -25,8 +25,8 @@ class LinkSetsDrawForm(FormBase):
         self.helper.layout = Layout(
             Row(
                 HTML(_("<div class='alert alert-info' role='alert'>Separate items by commas. e.g: Maria, David S, Leo, ...</div>")),
-                Field('set_1', css_class="tokenfield"),
-                Field('set_2', css_class="tokenfield"),
+                Field('set_1', css_class="eas-tokenfield"),
+                Field('set_2', css_class="eas-tokenfield"),
             ),
         )
 
