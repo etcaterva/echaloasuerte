a8e624437b8ac4e8a9fa11a2fa89ab370c010bcb
David Naranjo <dnaranjo89@gmail.com>
2015-07-16 01:08:30 +0200

    Add a context processor to send the Google Analytics ID to the templates
    Before it was harcoded

--- echaloasuerte/settings/common.py
+++ echaloasuerte/settings/common.py
@@ -171,7 +171,8 @@ TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
                                "django.core.context_processors.media",
                                "django.core.context_processors.static",
                                "django.core.context_processors.tz",
-                               "django.contrib.messages.context_processors.messages")
+                               "django.contrib.messages.context_processors.messages",
+                               "server.context_processors.google_analytics")
 
 ########## END APP CONFIGURATION
 
