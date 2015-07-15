65cc84706d82855181d3b972cbd072a6eb7e14c4
David Naranjo <dnaranjo89@gmail.com>
2015-07-16 00:47:30 +0200

    Add function to track events from the server

---
+++ web/google_analytics.py
@@ -0,0 +1,34 @@
+import logging
+from django.utils.http import urlencode
+import http.client
+from django.conf import settings
+
+LOG = logging.getLogger("echaloasuerte")
+
+def ga_track_event(category, action, event=None, label=None, value=None):
+    if not settings.GOOGLE_ANALYTICS_PROPERTY_ID:
+        return
+    if not action or not category:
+        LOG.warning("Error sending event to Google Analytics (Category={0}, Action={1})".format(category, action))
+        return
+
+    params_dict = {
+        'v': 1,
+        'tid': settings.GOOGLE_ANALYTICS_PROPERTY_ID,
+        'cid': '666',
+        't': 'event',
+        'ec': category,
+        'ea': action,
+        'ev': 0
+    }
+    if label:
+        params_dict['el'] = label
+    if event:
+        params_dict['t'] = event
+    if value:
+        params_dict['ev'] = value
+
+
+    params = urlencode(params_dict)
+    connection = http.client.HTTPConnection('www.google-analytics.com')
+    connection.request('POST', '/collect', params)
