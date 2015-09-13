e997852220183ef63ef338111b1c49efa088a1be
Mario Corchero <mariocj89@gmail.com>
2015-09-13 13:32:53 +0100

    Add logging of all requests

--- echaloasuerte/settings/common.py
+++ echaloasuerte/settings/common.py
@@ -147,6 +147,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'echaloasuerte.logging_mw.RequestLoggerMiddleware',
 )
 ########## END MIDDLEWARE CONFIGURATION
 
