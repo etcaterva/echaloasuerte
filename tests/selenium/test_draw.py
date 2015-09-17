bd7bba22a498496ad38c69e58723ea9ca64cb675
David Naranjo <dnaranjo89@gmail.com>
2015-09-17 17:31:37 +0200

    Comment tests

--- tests/selenium/test_draw.py
+++ tests/selenium/test_draw.py
@@ -13,6 +13,7 @@ class NormalDrawTest(BrowserStackTest):
         self.driver.get(self.base_url + "/")
 
     def test_coin(self):
+        """Selenium: Coin draw (normal draw)"""
         driver = self.driver
         draw_box = driver.find_element_by_id("coin-draw")
         draw_box.click()
@@ -24,6 +25,7 @@ class NormalDrawTest(BrowserStackTest):
         self.assertNotEqual([], result)
 
     def test_random_number_test(self):
+        """Selenium: Random Number draw (normal draw)"""
         driver = self.driver
         draw_box = driver.find_element_by_id("number-draw")
         draw_box.click()
@@ -35,6 +37,7 @@ class NormalDrawTest(BrowserStackTest):
         self.assertNotEqual([], result)
 
     def test_random_card(self):
+        """Selenium: Card draw (normal draw)"""
         driver = self.driver
         draw_box = driver.find_element_by_id("card-draw")
         draw_box.click()
@@ -46,6 +49,7 @@ class NormalDrawTest(BrowserStackTest):
         self.assertNotEqual([], result)
 
     def test_random_item(self):
+        """Selenium: Random item draw (normal draw)"""
         driver = self.driver
         draw_box = driver.find_element_by_id("item-draw")
         draw_box.click()
@@ -58,6 +62,7 @@ class NormalDrawTest(BrowserStackTest):
         self.assertNotEqual([], result)
 
     def test_dice(self):
+        """Selenium: Dice draw (normal draw)"""
         driver = self.driver
         draw_box = driver.find_element_by_id("dice-draw")
         draw_box.click()
@@ -69,6 +74,7 @@ class NormalDrawTest(BrowserStackTest):
         self.assertNotEqual([], result)
 
     def test_link_sets(self):
+        """Selenium: Link sets draw (normal draw)"""
         driver = self.driver
         draw_box = driver.find_element_by_id("link_sets-draw")
         draw_box.click()
@@ -82,6 +88,7 @@ class NormalDrawTest(BrowserStackTest):
         self.assertNotEqual([], result)
 
     def test_back_button(self):
+        """Selenium: Back button inside draws"""
         driver = self.driver
         driver.find_element_by_id("number-draw").click()
         self.wait()
