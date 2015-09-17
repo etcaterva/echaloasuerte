26a2ece2221440d70744f3bfa70462c8008d11af
David Naranjo <dnaranjo89@gmail.com>
2015-09-17 19:09:03 +0200

    Pass browserStack key as environmental variable

--- tests/selenium/browserstack_base.py
+++ tests/selenium/browserstack_base.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from echaloasuerte.settings.common import SITE_ROOT
 from server.mongodb.driver import MongoDriver
 
-LOCAL_TESTS = os.environ.get('RUN_TESTS_LOCALLY')
+BROWSERSTACK_KEY = os.environ.get('BROWSERSTACK_KEY')
 
 
 class BrowserStackTest(LiveServerTestCase):
@@ -22,34 +22,25 @@ class BrowserStackTest(LiveServerTestCase):
     def setUp(self):
         self.db = MongoDriver.instance()
         self.base_url = self.live_server_url
-        test_url_file = join(SITE_ROOT, 'secret_tests.txt')
-        try:
-            key = open(test_url_file).read().strip()
-        except IOError:
-            print("Impossible to load secret_tests.txt")
-        else:
-            test_url = "http://davidnaranjo1:{0}@hub.browserstack.com:80/wd/hub".format(key)
+        if BROWSERSTACK_KEY:
+            test_url = "http://davidnaranjo1:{0}@hub.browserstack.com:80/wd/hub".format(BROWSERSTACK_KEY)
             # Specify capabilities
             desired_cap = {'browser': 'Safari',
                            'browser_version': '8.0',
                            'os': 'OS X',
                            'os_version': 'Yosemite',
-                           'resolution': '1024x768'
+                           'resolution': '1024x768',
+                           'browserstack.local': True,
+                           'browserstack.debug': True
                            }
-            if LOCAL_TESTS is None:
-                desired_cap['browserstack.local'] = True
-                desired_cap['browserstack.debug'] = True
+            self.driver = webdriver.Remote(command_executor=test_url, desired_capabilities=desired_cap)
+        else:
+            # PhantomJS (Silent mode)
+            self.driver = webdriver.PhantomJS('C:/phantomjs/bin/phantomjs.exe')
 
-                # BrowserStack
-                self.driver = webdriver.Remote(command_executor=test_url, desired_capabilities=desired_cap)
-            else:
-                if LOCAL_TESTS == "silent":
-                    # PhantomJS (Silent)
-                    self.driver = webdriver.PhantomJS('C:/phantomjs/bin/phantomjs.exe')
-                else:
-                    # Firefox
-                    self.driver = webdriver.Firefox()
-                self.driver.set_window_size(1000, 900)
+            # Firefox (Graphic mode)
+            #self.driver = webdriver.Firefox()
+            self.driver.set_window_size(1000, 900)
 
     def tearDown(self):
         self.driver.quit()
