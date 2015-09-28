c5b29de8a1b1d0ada264babcfe17c92eb2c4ebfa
David Naranjo <dnaranjo89@gmail.com>
2015-09-28 16:51:45 +0200

    Create shared draw through API

--- static/js/public_draw_creator.js
+++ static/js/public_draw_creator.js
@@ -10,25 +10,53 @@ PublicDrawCreator.show_configure_step = function () {
     $('.step-configure').removeClass('hidden');
 };
 
-// Get all the messages of a public draw and refresh the chat board
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
 PublicDrawCreator.create_draw = function (){
     // Disable button to avoid duplicated submissions
     $('#publish').prop('disabled',true);
-    var form_fields = $('#draw-form').serialize();
-    form_fields += "&draw_type=" + PublicDrawCreator.draw_type;
-    $.post( PublicDrawCreator.url_create, form_fields)
-        .done(function( data ) {
-            // Since the form has been just validated, hide possible previous alerts
-            $('.step-configure .alert').hide();
+
+    // Serialize and clean the form
+    var form_fields = $('#draw-form').serializeObject();
+    delete form_fields["csrfmiddlewaretoken"];
+    delete form_fields["_id"];
+    form_fields["type"] = PublicDrawCreator.draw_type;
+    var data = JSON.stringify(form_fields);
+
+    $.ajax({
+        type : "POST",
+        contentType : 'application/json',
+        url: PublicDrawCreator.url_create,
+        data: data
+        }).done(function( data, textStatus, xhr ) {
+            // Get the url to the draw
+            var url_draw_api = xhr.getResponseHeader('Location');
+            var url_draw_web = url_draw_api.replace(/api\/v[\d\.]+\//g,'');
+
             // Present the link to the user
-            var draw_url = location.protocol + location.host + data.draw_url;
-            $('.url-share').val(draw_url);
+            $('.url-share').val(url_draw_web);
+
             // Set the link of the "Go to the draw" button
-            $('#go-to-draw').attr('href', data.draw_url);
+            $('#go-to-draw').attr('href', url_draw_web);
 
             PublicDrawCreator.show_spread_step();
         })
-        .fail(function (){
+        .fail(function (e){
             var $form = $('#draw-form');
             $form.attr("action", PublicDrawCreator.url_try);
             $form.submit();
