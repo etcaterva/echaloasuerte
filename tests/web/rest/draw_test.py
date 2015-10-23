16f5b97f3c5d2e527b2afc9fdf88a48754846981
David Naranjo <dnaranjo89@gmail.com>
2015-10-23 14:05:57 +0200

    Fixup unit test

--- tests/web/rest/draw_test.py
+++ tests/web/rest/draw_test.py
@@ -1539,14 +1539,14 @@ class DrawResourceChat_Test(ResourceTestCase):
         draw = self.draw
         resp = self.api_client.post(self.chat_uri(draw), data={
             "message": "chat message",
-            "user_alias": "anon user"
+            "anonymous_alias": "anon user"
         })
         print(resp)
         self.assertHttpOK(resp)
         chats = self.mongo.retrieve_chat_messages(draw.pk)
         self.assertEqual(1, len(chats))
         self.assertEqual(None, chats[0]["user"])
-        self.assertEqual("anon user", chats[0]["user_alias"])
+        self.assertEqual("anon user", chats[0]["anonymous_alias"])
 
     def test_post_chat(self):
         self.login()
