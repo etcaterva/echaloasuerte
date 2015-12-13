ab3fbacc09d14499d3771e6679ad67dbab172340
David Naranjo <dnaranjo89@gmail.com>
2015-12-13 22:06:03 +0100

    Remove sites package

--- web/sitemap.py
+++ web/sitemap.py
@@ -1,14 +1,6 @@
-from calendar import timegm
 from django.contrib import sitemaps
 import datetime
-from django.contrib.sitemaps.views import x_robots_tag
-from django.contrib.sites.models import Site
-from django.core.paginator import EmptyPage, PageNotAnInteger
 from django.core.urlresolvers import reverse
-from django.http import Http404
-from django.template.response import TemplateResponse
-from django.utils.http import http_date
-from django.utils import six
 from server.draw_factory import REGISTRY
 
 
@@ -46,33 +38,3 @@ class OthersSitemap(sitemaps.Sitemap):
 
     def location(self, obj):
         return reverse(obj)
-
-#
-# @x_robots_tag
-# def sitemap(request, sitemaps, template_name='sitemap.xml', content_type='application/xml'):
-#     req_protocol = request.scheme
-#     host = request.META.get('HTTP_HOST')
-#     req_site = Site(domain=host)
-#
-#     maps = list(six.itervalues(sitemaps))
-#     page = request.GET.get("p", 1)
-#
-#     urls = []
-#     for site in maps:
-#         try:
-#             if callable(site):
-#                 site = site()
-#             urls.extend(site.get_urls(page=page, site=req_site,
-#                                       protocol=req_protocol))
-#         except EmptyPage:
-#             raise Http404("Page %s empty" % page)
-#         except PageNotAnInteger:
-#             raise Http404("No page '%s'" % page)
-#     response = TemplateResponse(request, template_name, {'urlset': urls},
-#                                 content_type=content_type)
-#     if hasattr(site, 'latest_lastmod'):
-#         # if latest_lastmod is defined for site, set header so as
-#         # ConditionalGetMiddleware is able to send 304 NOT MODIFIED
-#         response['Last-Modified'] = http_date(
-#             timegm(site.latest_lastmod.utctimetuple()))
-#     return response
