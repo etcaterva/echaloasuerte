bec85af7c954ada55e349c9e02e576991c218efe
Mario Corchero <mariocj89@gmail.com>
2016-02-20 00:03:53 +0000

    Resize image to fit the screen

--- static/js/photo.js
+++ static/js/photo.js
@@ -3,11 +3,17 @@
   as stated in data-url and data-points
 **/
 function draw_canvas(canvas) {
+  var MAX_WIDTH = window.screen.availWidth / 4;
   var ctx = canvas[0].getContext('2d');
   var img = new Image();
   img.onload = function(){
     img_width = this.width;
     img_height = this.height;
+    if (img_width > MAX_WIDTH) {
+        var resize = img_width / MAX_WIDTH;
+        img_width = img_width / resize;
+        img_height = img_height / resize;
+    }
     canvas.attr('width', img_width);
     canvas.attr('height', img_height);
     ctx.drawImage(img, 0, 0, img_width, img_height);
