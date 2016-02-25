8939b724bb8c5328709c7610a7ce902520d65834
David Naranjo <dnaranjo89@gmail.com>
2016-02-25 23:22:15 +0100

    Resize photos based on parent container

--- static/js/photo.js
+++ static/js/photo.js
@@ -3,12 +3,17 @@
   as stated in data-url and data-points
 **/
 function draw_canvas(canvas) {
-  var MAX_WIDTH = window.screen.availWidth / 4;
+  var MAX_WIDTH = canvas.parent().width();
+  if (!canvas.is('#canvas-photo-main')){
+	  // If the canvas is in the history make it smaller to fit in the accordion
+	  MAX_WIDTH *= 0.8;
+  }
   var ctx = canvas[0].getContext('2d');
   var img = new Image();
   img.onload = function(){
-    img_width = this.width;
-    img_height = this.height;
+    var img_width = this.width;
+    var img_height = this.height;
+
     if (img_width > MAX_WIDTH) {
         var resize = img_width / MAX_WIDTH;
         img_width = img_width / resize;
