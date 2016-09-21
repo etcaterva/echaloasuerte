b9c388ba5de285e6766eda47504b3d14e82dfc9d
Pedro Alvarez <palvarez89@gmail.com>
2016-09-21 22:03:35 +0100

    Revert "Remove unused config (#677)"
    This reverts commit f6a1b00f54c91f50323bca13f874759d151da02c.
    
    The configuration file being removed is still in use in manage.py

---
+++ echaloasuerte/settings/develop.py
@@ -0,0 +1,96 @@
+from echaloasuerte.settings.common import *
+
+# WSGI_APPLICATION = '%s.wsgi.application' % SITE_NAME
+
+
+########## DEBUG CONFIGURATION
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+########## END DEBUG CONFIGURATION
+
+########## DATABASE CONFIGURATION
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': normpath(join(SITE_ROOT, 'default.db')),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+########## END DATABASE CONFIGURATION
+
+########## LOGGING CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#logging
+##########
+BASE_LOG_PATH = join(SITE_ROOT, 'logs/')
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s %(levelname)s %(filename)s %(lineno)s %(message)s',
+        },
+        'standard': {
+            'format': '[%(asctime)s] %(levelname)s %(name)s.%(module)s.%(funcName)s | %(message)s',
+            'datefmt': '%Y%m%d %H:%M:%S',
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+        'log_file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': BASE_LOG_PATH + 'echaloasuerte_log.txt',
+        },
+        'error_log_file': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': BASE_LOG_PATH + 'echaloasuerte_err.txt',
+        },
+        'debug_log_file': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': BASE_LOG_PATH + 'echaloasuerte_debug.txt',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'echaloasuerte': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
+########## END LOGGING CONFIGURATION
+
+# Mongo db configuration list
+MONGO_END_POINTS = [
+    {'host': 'localhost', 'port': 27017, 'database': 'develop'},
+]
