a81cf1b5f49b98e62c3f80198148fd62eaa9fa18
Mario Corchero <mariocj89@gmail.com>
2016-09-22 22:33:44 +0100

    Remove develop config file

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "echaloasuerte.settings.develop")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "echaloasuerte.settings.prod")
 
     from django.core.management import execute_from_command_line
 
